b7c4f506a4d67f4a235cae323123787e67b7f86a
Nghia-98 <hoangnghia.binhthuan@gmail.com>
2021-12-26 12:13:48 +0700

    working on shopping-cart screen

--- src/reduxStore/index.js
+++ src/reduxStore/index.js
@@ -9,10 +9,38 @@ const rootReducer = combineReducers({
   cartReducer: cartReducer,
 });
 
+const cartDataTest = [
+  {
+    productId: 5,
+    name: 'Hoa Giấy Bonsai',
+    category: 'Hoa',
+    location: 'Việt Nam',
+    price: 1999000,
+    countInStock: '6',
+    image: require('../assets/bonsai_1.jpg'),
+    description:
+      'Hoa giấy mọc thành một nhóm gồm 3 hoa và lá bắc, hình thành cụm ở phần cuối của nhánh. Lá bắc xếp hình tam giác, cách chăm sóc cây hoa giấy thuôn dài/trứng và nhọn đầu dài khoảng 3 – 5 cm. Bông hoa giấy thật sự nhỏ, mỗi cái gắn vào một lá bắc, hình ống phồng ở phần giữa.',
+    rating: 5,
+    qty: 3,
+  },
+  {
+    productId: 6,
+    name: 'Tùng Lá Kim',
+    category: 'Phong Thuỷ',
+    location: 'Việt Nam',
+    price: 1999000,
+    countInStock: '5',
+    image: require('../assets/bonsai_2.jpg'),
+    description:
+      'Cây tùng lá kim bonsai này được tạo dáng bay, tán hướng xuống qua miệng chậu đến 80 cm. Dáng này còn được gọi là dáng bán huyền, một dáng cơ bản và phổ biến trong nghệ thuật bonsai. Chậu cây tùng lá kim này có kích thược nhỏ, phù hợp trang trí nhiều nơi, dễ di chuyển, đặt để.',
+    rating: 4,
+    qty: 1,
+  },
+];
 // Todo: Change later
 const cartItemsLocalStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
-  : data.slice(5);
+  : cartDataTest;
 
 const shippingAdressLocalStorage = localStorage.getItem('shippingAddress')
   ? JSON.parse(localStorage.getItem('shippingAddress'))
--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -9,24 +9,63 @@ import AppText from '../components/AppText';
 import HeaderTab from '../components/HeaderTab';
 import colors from '../config/colors';
 import { AppNavigatorProps } from '../navigation/types';
+import numberFormat from '../util/formatNumberMoney';
 
 interface Props {
   navigation: StackNavigationProp<AppNavigatorProps>;
 }
 
-const CartScreen: React.FC<Props> = ({ navigation }) => {
+const CartScreen: React.FC<Props> = (props) => {
   const dispatch = useDispatch();
+
   const cartReducer = useSelector((state: any) => state.cartReducer);
-  console.log('CartScreen - cartReducer: ', cartReducer);
   const { cartItems } = cartReducer;
+  console.log('CartScreen - cartReducer: ', cartItems);
 
   return (
     <Screen>
-      <View style={{ marginTop: 20, paddingLeft: 16 }}>
+      <View style={{ marginTop: 20, paddingLeft: 22 }}>
         <HeaderTab>Giỏ Hàng</HeaderTab>
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <AppText>This is Shipping-Cart Screen</AppText>
+        {cartItems.length === 0 ? (
+          <View style={{ paddingVertical: 40, paddingHorizontal: 22 }}>
+            <View
+              style={{
+                backgroundColor: '#d2ebf5',
+                borderColor: 'c0e3f2',
+                height: 50,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <AppText style={{ color: '#10516c', fontWeight: 'bold' }}>
+                Your cart is emty
+              </AppText>
+            </View>
+          </View>
+        ) : (
+          <View style={{ paddingVertical: 40, paddingHorizontal: 22 }}>
+            {cartItems.map((cartItem: any) => {
+              return (
+                <View key={cartItem.productId}>
+                  <View style={{ marginBottom: 8 }}>
+                    <Image
+                      source={{ uri: cartItem.image }}
+                      style={{ width: '100%', height: 220, borderRadius: 20 }}
+                    />
+                  </View>
+                  <View style={{ marginBottom: 3 }}>
+                    <AppText>{cartItem.name}</AppText>
+                  </View>
+                  <View style={{ marginBottom: 16 }}>
+                    <AppText>$ {numberFormat(cartItem.price)}</AppText>
+                  </View>
+                </View>
+              );
+            })}
+          </View>
+        )}
       </ScrollView>
     </Screen>
   );
