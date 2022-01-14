4fbe31080fea9fbfe7d0584019bdc6dfd9b80ffb
Nghia-98 <hoangnghia.binhthuan@gmail.com>
2022-01-14 22:39:13 +0700

    show notificaion when shipping address is empty

--- src/screens/AddressScreen.tsx
+++ src/screens/AddressScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   Image,
   ScrollView,
@@ -7,18 +7,18 @@ import {
   View,
   TextInput,
   Text,
-  Picker,
-  Button,
-} from "react-native";
-import { saveShippingAddress } from "../actions/cartActions";
-import { StackNavigationProp } from "@react-navigation/stack";
-import { Divider } from "react-native-elements";
-import Screen from "./Screen";
-import AppText from "../components/AppText";
-import HeaderTab from "../components/HeaderTab";
-import colors from "../config/colors";
-import { AppNavigatorProps } from "../navigation/types";
-import numberFormat from "../util/formatNumberMoney";
+  Button
+} from 'react-native';
+import { Picker } from '@react-native-community/picker';
+import { saveShippingAddress } from '../actions/cartActions';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { Divider } from 'react-native-elements';
+import Screen from './Screen';
+import AppText from '../components/AppText';
+import HeaderTab from '../components/HeaderTab';
+import colors from '../config/colors';
+import { AppNavigatorProps } from '../navigation/types';
+import AwesomeAlert from 'react-native-awesome-alerts';
 
 interface Props {
   navigation: StackNavigationProp<AppNavigatorProps>;
@@ -27,26 +27,61 @@ interface Props {
 const AddressScreen: React.FC<Props> = ({ navigation }) => {
   const dispatch = useDispatch();
 
-  const [address, setAddress] = useState("");
-  const [selectedValueProvince, setSelectedValueProvince] = useState("TP HCM");
-  const [selectedValueDistrict, setSelectedValueDistrict] = useState("Q.1");
-  const [selectedValueWard, setSelectedValueWard] = useState("P.1");
+  const [address, setAddress] = useState('');
+  const [selectedValueProvince, setSelectedValueProvince] = useState('TP HCM');
+  const [selectedValueDistrict, setSelectedValueDistrict] = useState('Q.1');
+  const [selectedValueWard, setSelectedValueWard] = useState('P.1');
 
-  const Province = ["TP HCM", "Vũng tàu"];
+  const [showAlert, setShowAlert] = useState(false);
 
-  const District = ["Q.1", "Q.2", "Q.3"];
+  const hideAlert = () => {
+    setShowAlert(false);
+  };
 
-  const Ward = ["P.1", "P.2", "P.3"];
+  const startShowAlert = () => {
+    console.log('startShowAlert func - showAlert: ', showAlert);
+
+    setShowAlert(true);
+  };
+
+  const Province = ['TP HCM', 'Vũng tàu'];
+
+  const District = ['Q.1', 'Q.2', 'Q.3'];
+
+  const Ward = ['P.1', 'P.2', 'P.3'];
 
   const cartReducer = useSelector((state: any) => state.cartReducer);
   const { cartItems } = cartReducer;
-  console.log("CartScreen - cartReducer: ", cartItems);
+  console.log('CartScreen - cartReducer: ', cartItems);
 
   return (
-    <Screen style={{ backgroundColor: "#FFFFFF" }}>
+    <Screen style={{ backgroundColor: '#FFFFFF' }}>
       <View style={{ marginTop: 20 }}>
         <HeaderTab navigation={navigation}>Địa chỉ giao hàng</HeaderTab>
       </View>
+      <AwesomeAlert
+        show={showAlert}
+        showProgress={false}
+        title='Địa chỉ giao hàng'
+        message='Địa chỉ nhà không được để trống!'
+        closeOnTouchOutside={true}
+        closeOnHardwareBackPress={false}
+        showConfirmButton={true}
+        confirmText='OK'
+        confirmButtonColor='#DD6B55'
+        alertContainerStyle={{
+          zIndex: 999,
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          height: '100%!important'
+        }}
+        onConfirmPressed={() => {
+          hideAlert();
+        }}
+      />
       <View style={{ paddingVertical: 40, paddingHorizontal: 22 }}>
         <View style={styles.field}>
           <Text style={styles.label}>Thành phố</Text>
@@ -57,8 +92,8 @@ const AddressScreen: React.FC<Props> = ({ navigation }) => {
               setSelectedValueProvince(itemValue)
             }
           >
-            {Province.map((e) => (
-              <Picker.Item label={e} value={e} />
+            {Province.map((e, index) => (
+              <Picker.Item key={index} label={e} value={e} />
             ))}
           </Picker>
         </View>
@@ -86,7 +121,7 @@ const AddressScreen: React.FC<Props> = ({ navigation }) => {
             }
           >
             {Ward.map((e) => (
-              <Picker.Item label={e} value={e} />
+              <Picker.Item key={e} label={e} value={e} />
             ))}
           </Picker>
         </View>
@@ -97,30 +132,36 @@ const AddressScreen: React.FC<Props> = ({ navigation }) => {
             style={styles.input}
             onChangeText={setAddress}
             value={address}
-            placeholder="Địa chỉ nhà"
+            placeholder='Địa chỉ nhà'
             // keyboardType="numeric"
           />
         </View>
 
         <AppText
           onPress={() => {
-            navigation.navigate("PlaceOrder");
+            if (address != '') {
+              navigation.navigate('PlaceOrder');
 
-            console.log(Ward);
-            dispatch(
-              saveShippingAddress(
-                `${address}, ${selectedValueWard}, ${selectedValueDistrict}, ${selectedValueProvince}`
-              )
-            );
+              console.log(Ward);
+              dispatch(
+                saveShippingAddress(
+                  `${address}, ${selectedValueWard}, ${selectedValueDistrict}, ${selectedValueProvince}`
+                )
+              );
+            } else {
+              if (!showAlert) {
+                startShowAlert();
+              }
+            }
           }}
           style={{
             backgroundColor:
-              cartItems.length !== 0 ? colors.active : "#rgb(107,107,107)",
+              cartItems.length !== 0 ? colors.active : '#rgb(107,107,107)',
             color: colors.white,
             paddingHorizontal: 20,
             paddingVertical: 16,
             borderRadius: 30,
-            textAlign: "center",
+            textAlign: 'center'
           }}
         >
           LƯU ĐỊA CHỈ
@@ -131,10 +172,8 @@ const AddressScreen: React.FC<Props> = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  label:{
-
-    fontSize:18
-
+  label: {
+    fontSize: 18
   },
   input: {
     height: 40,
@@ -142,14 +181,14 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 10,
-    backgroundColor: "#FFFFFF",
+    backgroundColor: '#FFFFFF'
   },
   button: {
-    borderRadius: 10,
+    borderRadius: 10
   },
   field: {
-    marginBottom: 30,
-  },
+    marginBottom: 30
+  }
 });
 
 export default AddressScreen;
