4bd29c80411be107778315adb53516a89d37c925
Nghia-98 <hoangnghia.binhthuan@gmail.com>
2022-01-15 18:37:41 +0700

    Fix App background color

--- src/screens/Screen.tsx
+++ src/screens/Screen.tsx
@@ -4,20 +4,18 @@ import { SafeAreaView, StyleProp, StyleSheet, ViewStyle } from 'react-native';
 import colors from '../config/colors';
 
 interface Props {
-  style?: StyleProp<ViewStyle>
+  style?: StyleProp<ViewStyle>;
 }
 
 const Screen: React.FC<Props> = ({ children, style }) => (
-  <SafeAreaView style={[styles.container, style]}>
-    {children}
-  </SafeAreaView>
+  <SafeAreaView style={[styles.container, style]}>{children}</SafeAreaView>
 );
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.light,
+    backgroundColor: '#ffffff',
     flex: 1
   }
-}); 
+});
 
 export default Screen;
